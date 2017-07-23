a50ebc5b0569b5114a0948d645192e4cd34b5dec
hadiab <hadi12345>
2017-07-23 13:07:23 +0300

    Getting started with the policy

--- app/Group.php
+++ app/Group.php
@@ -5,9 +5,10 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use App\Plan;
 
-class Group extends Model
-{
-    public function plan(){
+class Group extends Model {
+
+    public function plan() {
         return $this->hasMany('App\Plan');
     }
- }
+    
+}
